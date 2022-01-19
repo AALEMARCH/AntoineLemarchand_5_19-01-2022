c12f453480e55de1dac43fab68a1030f84470cde
Antoine Lemarchand <lemarchand.antoine00@gmail.com>
2022-01-19 20:11:44 +0100

    ajoutt de la méthode pour récuperer un ID sur les pages produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,2 +1,17 @@
-const product = window.location.search;
+const product = window.location.search.split("?id=").join("");
 console.log(product);
+
+let eachProductData = [];
+
+const fetchEachProducts = async () => {
+  await fetch(`http://localhost:3000/api/products/${product}`)
+    .then((res) => res.json())
+    .then((data) => (eachProductData = data))
+    .catch((res) => {
+      alert("une erreur est survenue!");
+    });
+
+  console.log(eachProductData);
+};
+
+fetchEachProducts();
